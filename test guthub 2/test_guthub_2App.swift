525607f482acb78d5e73fcd0376ca16170bd0c73
alicesari <alicepsari@gmail.com>
2023-02-15 10:12:56 +1100

    Initial Commit

---
+++ test guthub 2/test_guthub_2App.swift
@@ -0,0 +1,17 @@
+//
+//  test_guthub_2App.swift
+//  test guthub 2
+//
+//  Created by Alice Purnama Sari on 15/2/2023.
+//
+
+import SwiftUI
+
+@main
+struct test_guthub_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
